b7458a717e27bd9241db317c5a3fdc7f2ccd7c95
akhuntsaria <akhuntsaria@icloud.com>
2024-11-10 10:53:24 +0100

    Better types, naming

--- main.cu
+++ main.cu
@@ -1,9 +1,11 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <cstddef>
+#include <cstdint>
 #include <cstdlib>
 #include <stdio.h>
 #include <string.h>
+#include <sys/types.h>
 #include <time.h>
 
 #define cudaCheck() { _cudaCheck(__FILE__, __LINE__); }
@@ -15,57 +17,43 @@ inline void _cudaCheck(const char *file, int line) {
     }
 }
 
-typedef unsigned char u8;
+const int buff_w = 63000,
+    buff_h = 63000,
+    win_w = 1500,
+    win_h = 800,
+    gliders = 10000,
+    guns = 1000,
+    pan_speed = 10;
 
-const int GUNS = 1000,
-    GLIDERS = 10000,
-    PAN_SPEED = 10;
-
-int buff_w = 2000,
-    buff_h = 1000,
-    window_w = 1500,
-    window_h = 800,
-    pan_x = 0,
+int pan_x = 0,
     pan_y = 0;
 
-u8 pause = 0,
+uint8_t pause = 0,
     quit = 0;
 
-size_t buff_size = (size_t)buff_w * buff_h,
-    window_size = (size_t)window_w * window_h;
+size_t buff_size = (size_t)buff_w * buff_h;
 
 dim3 block_size = dim3(32, 32, 1);
 dim3 grid_size = dim3((buff_w + block_size.x - 1) / block_size.x, (buff_h + block_size.y - 1) / block_size.y, 1);
 
 // Copy only what's shown, device to host
-void partial_cpy_dth(u8* host_buff, u8* dev_buff) {
-    size_t start_x = (buff_w - window_w) / 2 + pan_x,
-        start_y = (buff_h - window_h) / 2 + pan_y;
+void partial_cpy_dth(uint8_t* host_buff, uint8_t* dev_buff) {
+    size_t start_x = (buff_w - win_w) / 2 + pan_x,
+        start_y = (buff_h - win_h) / 2 + pan_y;
 
-    for (int y = 0; y < window_h; ++y) {
+    for (int y = 0; y < win_h; ++y) {
         size_t src_i = (start_y + y) * buff_w + start_x,
-            dst_i = y * window_w;
-        cudaMemcpy(&host_buff[dst_i], &dev_buff[src_i], window_w, cudaMemcpyDeviceToHost);
+            dst_i = y * win_w;
+        cudaMemcpy(&host_buff[dst_i], &dev_buff[src_i], win_w, cudaMemcpyDeviceToHost);
     }
 }
 
-void partial_cpy_htd(u8* dev_buff, u8* host_buff) {
-    size_t start_x = (buff_w - window_w) / 2 + pan_x,
-        start_y = (buff_h - window_h) / 2 + pan_y;
-
-    for (int y = 0; y < window_h; ++y) {
-        size_t src_i = y * window_w,
-            dst_i = (start_y + y) * buff_w + start_x;
-        cudaMemcpy(&dev_buff[dst_i], &host_buff[src_i], window_w, cudaMemcpyHostToDevice);
-    }
-}
-
-__global__ void update_cells_kernel(u8* buff, u8* buff_copy, int width, int height) {
+__global__ void update_cells_kernel(uint8_t* buff, uint8_t* buff_copy, int width, int height) {
     long x = blockIdx.x * blockDim.x + threadIdx.x,
         y = blockIdx.y * blockDim.y + threadIdx.y;
     if (x >= width || y >= height) return;
 
-    u8 live_ne = 0;
+    uint8_t live_ne = 0;
     for (int i = -1;i <= 1;i++) {
         for (int j = -1;j <= 1;j++) {
             long nx = x + i, ny = y + j;
@@ -74,7 +62,7 @@ __global__ void update_cells_kernel(u8* buff, u8* buff_copy, int width, int heig
         }
     }
 
-    u8 col;
+    uint8_t col;
     if (buff_copy[y * width + x] == 255) {
         col = live_ne == 2 || live_ne == 3 ? 255 : 0;;
     } else {
@@ -84,7 +72,7 @@ __global__ void update_cells_kernel(u8* buff, u8* buff_copy, int width, int heig
     buff[y * width + x] = col;
 }
 
-void dev_update_cells(u8* host_buff, u8* dev_buff, u8* dev_buff_copy) {
+void update_cells(uint8_t* host_buff, uint8_t* dev_buff, uint8_t* dev_buff_copy) {
     cudaEvent_t start, stop;
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
@@ -112,10 +100,10 @@ int rando(int min, int max) {
     return (rand() % (max - min + 1)) + min;
 }
 
-void initial_state(u8* dev_buff) {
+void initial_state(uint8_t* dev_buff) {
     srand(time(NULL));
 
-    u8 gun[64] = {
+    uint8_t gun[64] = {
         255,0,  255,0,  0,  0,  0,  0,
         0,  0,  255,0,  0,  0,  0,  0,
         0,  0,  0,  0,  255,0,  0,  0,
@@ -129,7 +117,7 @@ void initial_state(u8* dev_buff) {
     int border_h = buff_h / 50,
         border_w = buff_w / 50;
 
-    for (int figure = 0;figure < GUNS;figure++) {
+    for (int figure = 0;figure < guns;figure++) {
         int start_x = rando(border_w, buff_w - border_w),
             start_y = rando(border_h, buff_h - border_h);
 
@@ -140,7 +128,7 @@ void initial_state(u8* dev_buff) {
         }
     }
 
-    u8 glider1[9] = {
+    uint8_t glider1[9] = {
         0,  255,255,
         255,0,  255,
         0,  0,  255,
@@ -150,11 +138,11 @@ void initial_state(u8* dev_buff) {
         0,  255,255,
     };
 
-    for (int figure = 0;figure < GLIDERS;figure++) {
+    for (int figure = 0;figure < gliders;figure++) {
         int start_x = rando(border_w, buff_w - border_w),
             start_y = rando(border_h, buff_h - border_h);
 
-        u8* glider = rand() % 2 ? glider1 : glider2;
+        uint8_t* glider = rand() % 2 ? glider1 : glider2;
 
         for (int i = 0; i < 3; i++) {
             size_t src_i = i * 3,
@@ -167,13 +155,13 @@ void initial_state(u8* dev_buff) {
 void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods) {
     if (action == GLFW_PRESS || action == GLFW_REPEAT) {
         if (key == GLFW_KEY_LEFT) {
-            pan_x = pan_x - PAN_SPEED;
+            pan_x = pan_x - pan_speed;
         } else if (key == GLFW_KEY_RIGHT) {
-            pan_x = pan_x + PAN_SPEED;
+            pan_x = pan_x + pan_speed;
         } else if (key == GLFW_KEY_UP) {
-            pan_y = pan_y + PAN_SPEED;
+            pan_y = pan_y + pan_speed;
         } else if (key == GLFW_KEY_DOWN) {
-            pan_y = pan_y - PAN_SPEED;
+            pan_y = pan_y - pan_speed;
         } else if (key == GLFW_KEY_Q) {
             quit = 1;
         } else if (key == GLFW_KEY_P) {
@@ -183,15 +171,14 @@ void key_callback(GLFWwindow *window, int key, int scancode, int action, int mod
 }
 
 int main(void) {
-    printf("Buffer size: %zd\n", buff_size);
-    printf("Window data size: %zd\n", window_size);
+    printf("Buff size: %zd bytes\n", buff_size);
     printf("Block Size: (%d, %d, %d)\n", block_size.x, block_size.y, block_size.z);
     printf("Grid Size: (%d, %d, %d)\n", grid_size.x, grid_size.y, grid_size.z);
 
     if (!glfwInit()) return 1;
 
     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-    GLFWwindow* window = glfwCreateWindow(window_w, window_h, "Conway", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(win_w, win_h, "Conway", NULL, NULL);
     if (!window) return 1;
 
     glfwMakeContextCurrent(window);
@@ -202,8 +189,8 @@ int main(void) {
 
     glfwSetKeyCallback(window, key_callback);
 
-    u8 *host_buff, *dev_buff, *dev_buff_copy;    
-    cudaHostAlloc((void**)&host_buff, window_size, cudaHostAllocDefault);
+    uint8_t *host_buff, *dev_buff, *dev_buff_copy; // Column-major order 
+    cudaHostAlloc((void**)&host_buff, win_w * win_h, cudaHostAllocDefault);
     cudaMalloc(&dev_buff, buff_size);
     cudaMalloc(&dev_buff_copy, buff_size);
 
@@ -219,7 +206,7 @@ int main(void) {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-    int gen = 0; 
+    uint32_t gen = 0; 
     while (!glfwWindowShouldClose(window) && !quit) {
         glfwPollEvents();
 
@@ -230,7 +217,7 @@ int main(void) {
         printf("  pan y %d\n", pan_y); 
 
         glEnable(GL_TEXTURE_2D);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, window_w, window_h, 0, GL_RED, GL_UNSIGNED_BYTE, host_buff);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, win_w, win_h, 0, GL_RED, GL_UNSIGNED_BYTE, host_buff);
 
         glBegin(GL_QUADS);
             glTexCoord2f(.0f, .0f); glVertex2f(-1.f, -1.f);
@@ -242,7 +229,7 @@ int main(void) {
         glDisable(GL_TEXTURE_2D);
         glfwSwapBuffers(window);
 
-        dev_update_cells(host_buff, dev_buff, dev_buff_copy);
+        update_cells(host_buff, dev_buff, dev_buff_copy);
     }
 
     cudaFree(dev_buff);
